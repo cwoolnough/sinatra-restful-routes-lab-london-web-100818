b6f1225d7cb0e2adc95eb9b4258240e32a40259a
github username <github email address>
2018-11-01 18:29:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,58 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
     erb :index
   end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    p params
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(params)
+    redirect "/recipes/#{@recipe.id}"
+  end 
+
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find(params[:id])
+    Recipe.destroy(params[:id])
+    redirect "/recipes"
+  end
+
+
+
+
+
+
+ 
+
+
+
+
+
+
 end
